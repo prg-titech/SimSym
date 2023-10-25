ccdaac808b83432c23bc9afdcf8eb865a3cd37de
Kosuke <mail@r.flutcla.tech>
2023-10-25 19:51:36 +0900

    package としての体裁を整えた

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='simsym',
+    version='0.0.1',
+    description='Environment which can be used to teach physics to high school students',
+    author='Kosuke Kiuchi',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+)
